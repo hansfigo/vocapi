3e5e1f0c7ae7b0b125bf89e5ec66e73f12328b89
hansfigo <hambasahaya0303@gmail.com>
2025-02-10 04:46:26 +0700

    move test folder to root project

--- test/albums.test.ts
+++ test/albums.test.ts
@@ -1,9 +1,9 @@
 import { expect, test } from "vitest";
-import albumServices from "../services/album/album.services";
+import albumServices from "../src/services/album/album.services";
 
 test("getAlbums", async () => {
-    const albums = await albumServices.searchAlbums({ query: "FUFUGAZER" });
-    console.log(albums, "FUFUGAZER ");
+    const albums = await albumServices.searchAlbums({ query: "Skatsune Miku" });
+    console.log(albums, "FUFUGAZER");
     expect(albums).toBeDefined();
     expect(albums.items.length).toBeGreaterThan(0);
 });
