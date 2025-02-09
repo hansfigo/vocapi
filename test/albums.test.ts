4275a10ecb11339be4296e6a859b870967c85919
hansfigo <hambasahaya0303@gmail.com>
2025-02-10 05:29:52 +0700

    update test

--- test/albums.test.ts
+++ test/albums.test.ts
@@ -1,8 +1,9 @@
 import { expect, test } from "vitest";
-import albumServices from "../src/services/album/album.services";
+
+import Vocapi from "../src/index";
 
 test("getAlbums", async () => {
-    const albums = await albumServices.searchAlbums({ query: "Skatsune Miku" });
+    const albums = await Vocapi.album.search({ query: "Skatsune Miku" });
     console.log(albums, "FUFUGAZER");
     expect(albums).toBeDefined();
     expect(albums.items.length).toBeGreaterThan(0);
@@ -10,6 +11,6 @@ test("getAlbums", async () => {
 
 
 test("getAlbumsByID", async () => {
-    const album = await albumServices.getAlbum(1);
+    const album = await Vocapi.album.get(1);
     expect(album).toBeDefined();
 });
